d3992aa61ae2ab8c053c69076722ea47903aecae
Levon Matiros <levon.matiros@elev.ga.ntig.se>
2023-09-01 10:35:20 +0200

    srhetyj

---
+++ ConsoleApp1/Monster.cs
@@ -0,0 +1,18 @@
+namespace Bashamonster;
+public class Gubbe
+{
+    public float maxHp = 100;
+    public string  name;
+    public float hp;
+    public float mass = 80;
+
+    public Gubbe()
+    {
+        ResetHp();
+    }
+
+    public void ResetHp()
+    {
+        hp = maxHp;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Bashamonster;
+
+Gubbe martin = new();
+Gubbe micke = new();
+
+//martin.hp = 100;
+//micke.hp = 60;
+
+
+Console.ReadLine();
